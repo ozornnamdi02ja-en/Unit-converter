23cc14d9bf5c40e15214eb8051b56d4679e59b4f
Danlozy <ozornnamdi02ja@nsuk.edu.ng>
2026-08-15 10:14:04 +0100

    Updating

--- app/manifest.ts
+++ app/manifest.ts
@@ -5,10 +5,10 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "Universal Unit Converter",
     short_name: "Unit Converter",
     description:
-      "A universal unit converter for mathematics, physics, chemistry, computing and everyday measurements.",
+      "A universal unit converter for mathematics, physics, chemistry, computing , biology and everyday measurements.",
     start_url: "/",
     display: "standalone",
-    background_color: "#ffffff",
+    background_color: "black",
     theme_color: "#166534",
     icons: [
       {
